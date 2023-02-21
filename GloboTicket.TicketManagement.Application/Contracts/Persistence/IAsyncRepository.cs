f4e6db22453bef79c3150b207ec81773c525f557
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-21 18:55:31 +0100

    Designing the Application Project.

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        
+    }
+}
