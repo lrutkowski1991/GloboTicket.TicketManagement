a40819ce6beac3a42e4b53f4384b28625a01b54a
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-24 21:43:18 +0100

    Organizing the code using Features.

--- GloboTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -4,5 +4,6 @@ namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
 {
     public interface ICategoryRepository : IAsyncRepository<Category>
     {
+        Task<List<Category>> GetCategoriesWithEvents(bool includeHistory);
     }
 }
--- GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
@@ -12,6 +14,8 @@ namespace GloboTicket.TicketManagement.Application.Features.Profiles
             CreateMap<Event, EventListVM>().ReverseMap();
             CreateMap<Event, EventDetailVM>().ReverseMap();
             CreateMap<Category, CategoryDto>();
+            CreateMap<Category, CategoryEventListVM>();
+            CreateMap<Category, CategoryListVM>();
         }
     }
 }
