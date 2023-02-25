b70f9eb676fcf5b9941c7da7155f260a2f87820d
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-02-25 12:53:57 +0100

    Using commands to create a New Entity.

--- GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
@@ -16,6 +19,10 @@ namespace GloboTicket.TicketManagement.Application.Features.Profiles
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryEventListVM>();
             CreateMap<Category, CategoryListVM>();
+
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, DeleteEventCommand>().ReverseMap();
         }
     }
 }
