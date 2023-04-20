de53c1b8f0dfa167f569f041c162f3b5082e35d2
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-04-20 18:47:18 +0200

    Create the Unit test for application code.

---
+++ GloboTicket.TicketManagement.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
