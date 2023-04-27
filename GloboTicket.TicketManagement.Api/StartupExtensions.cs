748ddcbe08bfedf5c0877f6be4845d8f15e4ac03
lrutkowski1991 <lrutkowski1991@gmail.com>
2023-04-27 12:49:39 +0200

    Create Unit Tests for the Application Core.

--- GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,5 +1,7 @@
-﻿using GloboTicket.TicketManagement.Api.Utility;
+﻿using GloboTicket.TicketManagement.Api.Services;
+using GloboTicket.TicketManagement.Api.Utility;
 using GloboTicket.TicketManagement.Application;
+using GloboTicket.TicketManagement.Application.Contracts;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +19,7 @@ namespace GloboTicket.TicketManagement.Api
             builder.Services.AddApplicationServices();
             builder.Services.AddInfrastructureServices(builder.Configuration);
             builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddScoped<ILoggedInUserService, LoggedInUserService>();
 
             builder.Services.AddControllers();
 
